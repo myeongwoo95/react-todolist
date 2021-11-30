e7bd38d3e0822d0beff6deb092d051235112292c
walle <dlauddn95@gmail.com>
2021-11-30 17:09:57 +0900

    :>

---
+++ src/TodoContext.js
@@ -0,0 +1,68 @@
+import React, {useReducer} from 'react';
+
+const initialTodos = [
+    {
+        id: 1,
+        text: "프로젝트 생성하기",
+        done: true
+    },
+    {
+        id: 2,
+        text: "컴포넌트 스타일링 하기",
+        done: true
+    },
+    {
+        id: 3,
+        text: "Context 만들기",
+        done: false
+    },
+    {
+        id: 4,
+        text: "기능 구현하기",
+        done: false
+    },
+]
+
+/*
+    Create
+    Toggle
+    Remove
+*/
+function todoReducer(state, action){
+    switch(action.type){
+        case 'CREATE':
+            return state.concat(action.todo);
+        case 'TOGGLE':
+            return state.map(
+                todo => todo.id === action.id ? { ...todo, done: !todo.done} : todo
+            );
+        case 'REMOVE':
+            return state.filter(todo => todo.id !== action.id);
+        default:
+            throw new Error(`Unhandled action type: ${action.type}`)
+    }
+}
+
+const TodoStateContext = React.createContext();
+const TodoDispatchContext = React.createContext();
+
+export function TodoProvider({ children}){
+    const [state, dispatch] = useReducer(todoReducer, initialTodos);
+    return (
+        <TodoStateContext.Provider value={state}>
+            <TodoDispatchContext.Provider value={dispatch}>
+                {children}
+            </TodoDispatchContext.Provider>
+        </TodoStateContext.Provider>
+    )
+}
+
+function TodoContext(){
+    return(
+        <div>
+            
+        </div>
+    );
+}
+
+export default TodoContext;
